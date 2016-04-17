7164c962057a02ca6a590d2e27f25f7f2138f5e9
Steve Sunderland <stevesunderland@gmail.com>
2016-04-17 16:55:57 -0700

    add related posts and portfolio pages to services pages

--- page.php
+++ page.php
@@ -27,18 +27,32 @@ $other_pages = array(2381,6444);
 if ( in_array($parent, $landing_pages) || in_array($post->ID, $other_pages) ) {
      array_unshift($templates, 'templates/page-services-single.twig');
 
+
+    $related_ids = $post->related_portfolio_entries;
+
+
      // RELATED WORK
 	$args = array(
 	    'post_type' => 'portfolio',
-	    'posts_per_page' => 3
+	    'posts_per_page' => 3,
+	    'tax_query' => array(
+	    	array(
+	    		'taxonomy' => 'portfolio_entries',
+	    		'field' => 'id',
+	    		'terms' => $related_ids
+	    	)
+	    )
 	);
+
 	$context['related_work'] = Timber::get_posts($args);
 	// $context['portfolio_categories'] = Timber::get_terms('portfolio_entries');
 
+	$related_ids = $post->related_posts;
 	// RELATED POSTS
 	$args = array(
 	    'post_type' => 'post',
-	    'posts_per_page' => 3
+	    'posts_per_page' => 3,
+	    'cat' => $related_ids
 	);
 	$context['related_articles'] = Timber::get_posts($args);
 }
