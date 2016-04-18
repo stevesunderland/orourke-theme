bc4319cf3e36ebeaa5bb184165a3d3f17ebaa13c
Steve Sunderland <stevesunderland@gmail.com>
2016-04-17 22:47:29 -0700

    get intel categories from wordpress

--- page-intel.php
+++ page-intel.php
@@ -6,13 +6,28 @@ $context['post'] = $post;
 // BLOG POSTS
 $args = array(
     'post_type' => 'post',
-    'posts_per_page' => 9
+    // 'posts_per_page' => 9
+    'posts_per_page' => 90
 );
 $context['posts'] = Timber::get_posts($args);
 
 
+
+
+
 // CATEGORIES
-$context['categories'] = array('Blog Posts', 'Case Studies', 'White Papers');
+
+$categories = array();
+
+foreach ($post->intel_categories as $key => $value) {
+	// echo($value);
+	$category = get_category( $value );
+	// print_r($category);
+	array_push($categories, $category);
+}
+
+// $context['categories'] = array('Blog Posts', 'Case Studies', 'White Papers');
+$context['categories'] = $categories;
 
 
 Timber::render(array('templates/page-' . $post->post_name . '.twig', 'templates/page.twig'), $context);
